1086052c04fdafccee26802bddef2591017a3bf0
王叨叨 <hi@wangdaodao.com>
2020-09-24 09:11:26 +0800

    add：ColorPicker新增append-to-body属性

--- packages/color-picker/src/main.vue
+++ packages/color-picker/src/main.vue
@@ -48,7 +48,16 @@
       disabled: Boolean,
       size: String,
       popperClass: String,
-      predefine: Array
+      predefine: Array,
+      appendToBody: {
+        type: Boolean,
+        default: false
+      },
+    },
+    provide() {
+      return {
+        appendToBody: this.appendToBody
+      };
     },
 
     inject: {
