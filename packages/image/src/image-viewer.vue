fafe16fbf07ae4dfad400aa6bcef4023d7f5015b
王叨叨 <hi@wangdaodao.com>
2021-06-11 14:29:58 +0800

    Image:fixed iframe

--- packages/image/src/image-viewer.vue
+++ packages/image/src/image-viewer.vue
@@ -222,6 +222,7 @@ export default {
     },
     handleMouseDown(e) {
       if (this.loading || e.button !== 0) return;
+      off(document, 'mousemove', this._dragHandler);
       const { offsetX, offsetY } = this.transform;
       const startX = e.pageX;
       const startY = e.pageY;
